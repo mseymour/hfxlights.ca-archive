88214e8bc10a985da21d84d8aa4a43ebfe65ae12
Mark Seymour <mark.seymour.ns@gmail.com>
2017-11-19 19:19:24 -0400

    Add sluggable trait to Tag

--- app/Tag.php
+++ app/Tag.php
@@ -3,15 +3,32 @@
 namespace App;
 
 use App\Place;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    use Sluggable;
+
     /**
      * Get all of the places that are assigned this tag.
      */
     public function places()
     {
         return $this->morphedByMany(Place::class, 'taggable');
+    }
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
     }
 }
