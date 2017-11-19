e8789aa813c69e775def42de135011fa96432499
Mark Seymour <mark.seymour.ns@gmail.com>
2017-11-19 19:19:24 -0400

    Import basic Bootstrap 4 dependencies and set up basic structure for Sass

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,4 @@
 
-window._ = require('lodash');
-
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
@@ -8,9 +6,7 @@ window._ = require('lodash');
  */
 
 try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
+    require('bootstrap');
 } catch (e) {}
 
 /**
@@ -18,8 +14,6 @@ try {
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.autoload({
+    lodash: ['window._'],
+    axios: ['window.axios'],
+    'jquery/dist/jquery.slim': ['$', 'jQuery', 'window.jQuery'],
+    'popper.js/dist/umd/popper.js': ['Popper']
+  })
+   .js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
