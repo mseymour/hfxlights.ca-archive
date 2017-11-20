baccb473afbe1faea53bebbc96aac7f3af32fc4f
Mark Seymour <mark.seymour.ns@gmail.com>
2017-11-19 22:12:41 -0400

    Basic filter interaction styles

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,9 +2,9 @@
     <a class="overlay-nav__home" href="{{ url('/') }}"><span>{{ config('app.name', 'HFXLights') }}</span></a>
     <div class="overlay-nav__search filter" data-filter>
         <div class="filter__options">
-            <button type="button" class="filter__option"><span class="sr-only">Search for places</span> Near Me</button>
+            <button type="button" class="filter__option filter__button"><span class="sr-only">Search for places</span> Near Me</button>
             <form class="filter__option">
-                <input class="filter__search" type="search" placeholder="Search" aria-label="Search for places by address">
+                <input class="filter__search filter__button" type="search" placeholder="Search" aria-label="Search for places by address">
                 <button tabindex="-1" class="filter__submit" type="submit">Search</button>
             </form>
         </div>
