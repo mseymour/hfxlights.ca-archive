e49cd7dd3a194982aa6fc64ffaa6028a87e65773
Mark Seymour <mark.seymour.ns@gmail.com>
2017-11-28 14:09:04 -0400

    Add "Add Place" button to home
    Will do some juggling around with class names and structure later to fit in with the multi-function bar, or will develop a better UI structure.

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -6,7 +6,8 @@
     <div id="mapHud" class="map__hud">
         <div class="map__filter filter" data-filter>
             <div class="filter__options">
-                <button type="button" class="filter__option filter__button filter__button--geo"><span class="sr-only">Search for</span> Places Near Me</button>
+                <button type="button" class="filter__option filter__button filter__button--geo">(svg Search icon) <span class="filter__label"><span class="sr-only">Search for</span> Places Near Me</span></button>
+                <button type="button" class="filter__button add-place">(svg Search icon) <span class="filter__label">Add Place</span></button>
                 <form class="filter__option filter__option--address" action="{{ route('places.search') }}" method="post">
                     <input name="q" class="filter__search filter__button" type="search" placeholder="Search Places" aria-label="Search for places by address">
                     <button tabindex="-1" class="filter__submit" type="submit">Search Places</button>
