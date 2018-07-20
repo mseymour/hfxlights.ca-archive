2f4e3fe5ba50bdec896cba872ddd475dbb7d55ce
Mark Seymour <mark.seymour.ns@gmail.com>
2018-07-19 22:59:19 -0300

    Setup

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,23 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.webpackConfig({
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'vendor')],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+      },
+    ],
+  }
+});
+
+mix.react('resources/assets/js/app.js', 'public/js')
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .version()
+   .sourceMaps();
